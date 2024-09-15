5058cbbf83052582e3e622dafde34a70fd330342
= <kilualthaus@hotmail.com>
2024-09-15 14:34:03 +0200

    wörter uhr swiss german

---
+++ index.js
@@ -0,0 +1,101 @@
+function init() {
+    console.log('init');
+    run();
+}
+
+function run() {
+    console.log('run');
+    setInterval(runner, 1000);
+}
+
+async function runner() {
+    /*
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    console.log(`width: ${width}`);
+    console.log(`height: ${height}`);
+    if (width > height) {
+        width = height;
+    } else if ( width < height) {
+        height = width;
+    }
+    console.log(`width: ${width}`);
+    console.log(`height: ${height}`);
+    //window.resizeTo(height, width);
+     */
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    let hour = now.getHours();
+    let minutes = roundMinutes(now.getMinutes());
+    hour = hour % 12;
+    hour = hour ? hour : 12;
+    if (minutes >= 23 && minutes <= 59) {
+        hour++;
+    }
+    printHour(hour);
+    printMinutes(minutes);
+    printAfterOrBefore(minutes);
+    console.log(`Stunden: ${hour}`);
+    console.log(`Minuten: ${minutes}`);
+    console.log(`Uhrzeit: ${time}`);
+}
+
+function roundMinutes(minutes) {
+    if (minutes <= 2) return 0;
+    else if (minutes <= 7) return 5;
+    else if (minutes <= 12) return 10;
+    else if (minutes <= 17) return 15;
+    else if (minutes <= 22) return 20;
+    else if (minutes <= 27) return 25;
+    else if (minutes <= 32) return 30;
+    else if (minutes <= 37) return 35;
+    else if (minutes <= 42) return 40;
+    else if (minutes <= 47) return 45;
+    else if (minutes <= 52) return 50;
+    else if (minutes <= 57) return 55;
+    else return 0;
+}
+
+function printAfterOrBefore(minutes) {
+    if (minutes === 0 || minutes === 30) {
+        elementOff('.before');
+        elementOff('.after');
+    } else if (minutes >= 1 && minutes <= 20) {
+        elementOnOff('.after', '.before');
+    } else {
+        elementOnOff('.before', '.after');
+    }
+}
+
+function printHour(hour) {
+    elementOff(`.hour`);
+    elementOn(`.hour-${hour}`);
+}
+
+function printMinutes(minutes) {
+    elementOff(`.minutes`);
+    elementOn(`.minutes-${minutes}`);
+}
+
+function elementOff(selector) {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.remove('on');
+    });
+}
+
+function elementOn(selector) {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.add('on');
+    });
+}
+
+function elementOnOff(selectorOn, selectorOff) {
+    document.querySelectorAll(selectorOn).forEach(element => {
+        element.classList.add('on');
+    });
+    document.querySelectorAll(selectorOff).forEach(element => {
+        element.classList.remove('on');
+    });
+}
+
+init();
